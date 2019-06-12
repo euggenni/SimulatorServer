de874795b1f0c974584b1f9959f2f18500867afc
Evgeny <31629761+euggenni@users.noreply.github.com>
2019-06-12 20:40:21 +0400

    v 0.3

---
+++ Server_proba/Assets/Scripts/Process/ConnectStudent.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectStudent
+{
+    private string computer;
+    private string student;
+    private string state;
+
+    public string Computer
+    {
+        get
+        {
+            return computer;
+        }
+
+        set
+        {
+            computer = value;
+        }
+    }
+
+    public string Student
+    {
+        get
+        {
+            return student;
+        }
+
+        set
+        {
+            student = value;
+        }
+    }
+
+    public string State
+    {
+        get
+        {
+            return state;
+        }
+
+        set
+        {
+            state = value;
+        }
+    }
+
+    public ConnectStudent()
+    {
+        this.Computer = "Unknown";
+        this.Student = "Неизвестный";
+        this.State = "Ожидает";
+    }
+
+    public ConnectStudent(string Computer, string Student, string State)
+    {
+        this.Computer = Computer;
+        this.Student = Student;
+        this.State = State;
+    }
+}
---
+++ Server_proba/Assets/Scripts/Process/ConnectStudentManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectStudentManager : MonoBehaviour
+{
+    public UnityUITable.Table Table; //таблица с данными
+    public static List<ConnectStudent> ConnectStudents = new List<ConnectStudent>();
+
+
+    public void UpdateTable()
+    {
+        Table.UpdateContent();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ConnectStudents.Add(new ConnectStudent("K-333-2", "Петров Петр Петрович", "Ожидает"));
+        ConnectStudents.Add(new ConnectStudent("K-333-3", "Сидоров Сидр Сидорович", "Ожидает"));
+        ConnectStudents.Add(new ConnectStudent("K-333-5", "Иванов Иван Иванович", "В процессе"));
+        Invoke("UpdateTable", 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
