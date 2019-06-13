a670be77480d2ab3f7dcad92878c2680d39fea33
Evgeny <31629761+euggenni@users.noreply.github.com>
2019-06-13 06:06:34 +0400

    v 0.3.5

--- Server_proba/Assets/Scripts/Result/ResultManager.cs
+++ Server_proba/Assets/Scripts/Result/ResultManager.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class ResultManager : MonoBehaviour
 {
     public UnityUITable.Table Table; //таблица с данными
+    public UnityUITable.Table Table2; //таблица с данными
     public List<Result> Results = new List<Result>();
+    public List<Result> AvgNotes = new List<Result>();
 
     public void UpdateTable()
     {
         Table.UpdateContent();
+        Table2.UpdateContent();
     }
     // Start is called before the first frame update
     void Start()
@@ -17,6 +20,9 @@ public class ResultManager : MonoBehaviour
         Results.Add(new Result(1, "K-333-2", "Петров Петр Петрович", "Хорошо"));
         Results.Add(new Result(2, "K-333-3", "Сидоров Сидр Сидорович", "Удовлетв"));
         Results.Add(new Result(3, "K-333-5", "Иванов Иван Иванович", "Хорошо"));
+        AvgNotes.Add(new Result(3, "K-333-5", "Петров Петр Петрович", "4.0"));
+        AvgNotes.Add(new Result(3, "K-333-5", "Сидоров Сидр Сидорович", "3.0"));
+        AvgNotes.Add(new Result(3, "K-333-5", "Иванов Иван Иванович", "4.0"));
         Invoke("UpdateTable", 1);
     }
 
